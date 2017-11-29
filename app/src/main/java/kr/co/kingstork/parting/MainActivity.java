d352796b79d1df8b6972b86604c6e0c74a47b9d6
KingStork <hsodragon@naver.com>
2017-11-29 13:15:02 +0900

    add PeopleFragment

--- app/src/main/java/kr/co/kingstork/parting/MainActivity.java
+++ app/src/main/java/kr/co/kingstork/parting/MainActivity.java
@@ -3,11 +3,15 @@ package kr.co.kingstork.parting;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import kr.co.kingstork.parting.fragment.PeopleFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getFragmentManager().beginTransaction().replace(R.id.mainactivity_framelayout,new PeopleFragment()).commit();
     }
 }
